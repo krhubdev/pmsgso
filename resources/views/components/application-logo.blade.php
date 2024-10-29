4ac7978ee0d07a7f83c30002ea607c9bb7322692
Kent Russell N. Casino <dev.kiiao@gmail.com>
2024-10-29 14:54:44 +0800

    Version 1.1

--- resources/views/components/application-logo.blade.php
+++
@@ -1,1 +0,0 @@
-<img src="/images/logo.png" style="height:150px;" alt=""> 
--- resources/views/components/authentication-card-logo.blade.php
+++
@@ -1,8 +0,0 @@
-<a href="/">
-    <center>
-    <img src="/images/logo.png" style="height:250px;" alt=""> 
-    <h1 class="text-2xl font-medium text-gray-900">
-        <b>Property Management System (GSO)</b> <br>
-        Municipality of Tagoloan, Misamis Oriental
-</center>
-</a>
